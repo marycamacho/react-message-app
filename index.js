90d6028802a7b11230dc1ff47775c8938cbb4976
Mary Camacho <mary@joyasolutions.com>
2016-05-07 10:52:41 -0600

    updated to use userdata saved in JSON and to validate for multiple users

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 ;(function(){
 	"use strict";
 	
@@ -68,6 +69,7 @@
 	// put this elsewhere and hook extend this so it is more secure (in config.js) and allows multiple people not hardcoded in the end
 	function logInUser(username, password) {
 		if (username == "mary" && password == "password") {
+			//stuff the username into the session as a variable
 			return true;
 		}
 		return false;
@@ -96,7 +98,139 @@
 	});
 
 }());
+*/
 
+;
+(function () {
+	"use strict";
+
+	var PORT = 3000;
+
+	var fs = require('fs');
+
+	var express = require('express');
+	var bodyParser = require('body-parser');
+	var cookieParser = require('cookie-parser');
+	var expressSession = require('express-session');
+	var fs = require('fs');
+
+	var config = require('./config.js');
+
+	var app = express();
+
+	/*MiddleWare*/
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(cookieParser());
+	app.use(expressSession({
+		secret: config.secret,
+		resave: true,
+		saveUninitialized: true
+	}));
+
+	//the following is used to bring password data into app
+	var file = __dirname + '/data/users.json';
+	var parsedData;
+
+	fs.readFile(file, 'utf8', function (err, data) {
+		if (err) {
+			console.log('Error: ' + err);
+			return;
+		}
+
+		parsedData = JSON.parse(data);
+		console.log(parsedData);
+
+	});
+
+	var messages = ["This is a message", "This is another message"];
+
+	/*Routes*/
+
+	app.get("/", function (req, res) {
+		if (!req.session.username) {
+			res.redirect("/login");
+			return;
+		}
+		res.sendFile(__dirname + '/public/index.html');
+	});
+
+	app.get("/messages", function (req, res) {
+		if (!req.session.username) {
+			res.send("[]");
+			return;
+		}
+		res.send(JSON.stringify(messages));
+	});
+
+
+	app.post("/messages", function (req, res) {
+		if (!req.session.username) {
+			res.send("error");
+			return;
+		}
+		if (!req.body.newMessage) {
+			res.send("error");
+			return;
+		}
+		messages.push(req.body.newMessage);
+		res.send("success");
+	});
+
+	//*login routes and functions
+
+	function logInUser(username, password) {
+		//Need to identify incoming data and then loop through or compare to parsed data looking match
+		var foundUser = parsedData.find(function(user){
+
+			if (user.username === username) {
+				console.log(user);
+				return true
+			}
+			return false
+		});
+		if (foundUser) {
+			console.log("success!");
+			return foundUser.password === password
+		} else if (username == "guest" && password == "guest") {
+			console.log("success!");
+			return true;
+		}
+		return false;
+	}
+
+	app.get("/login", function (req, res) {
+		res.sendFile(__dirname + '/public/login.html');
+	});
+
+	app.post("/login", function (req, res) {
+		if (req.body.username && req.body.password) {
+			if (logInUser(req.body.username, req.body.password)) {
+				req.session.username = req.body.username;
+				res.redirect("/");
+				return
+			}
+		}
+		res.redirect("/login");
+	});
+
+	/*Always put last because it is sequential*/
+
+	app.use(express.static('public'));
+
+	app.use(function (req, res, next) {
+		res.status(404);
+		res.send("File not found");
+	});
+
+	app.listen(PORT, function () {
+		console.log("server started on port " + PORT);
+	});
+
+}());
 
 
 
